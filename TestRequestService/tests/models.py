e92c049c2065babbb8e69a775cca163d6455c05b
Andrew Robles <andrewrobles@gmail.com>
2020-09-23 08:12:54 -0700

    Implement Test, Question, Category, and Difficulty models

--- TestRequestService/tests/models.py
+++ TestRequestService/tests/models.py
@@ -1,4 +1,27 @@
 from django.db import models
 
+class Test(models.Model):
+    """
+    Needs inputs and expected fields
+    """
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE
+    )
+
 class Question(models.Model):
-    pass
+    description = models.TextField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE
+    )
+    difficulty = models.ForeignKey(
+        Difficulty,
+        on_delete=models.CASCADE
+    )
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+class Difficulty(model.Model):
+    name = models.CharField(max_length=50)
