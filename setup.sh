9d59ebd8f686daef2fd0eb0ab97f1e7174dcf6cf
Andrew Robles <andrewroblesdev@gmail.com>
2020-10-01 06:53:44 -0700

    Rename setup shell script

---
+++ setup.sh
@@ -0,0 +1,2 @@
+docker-compose run web python manage.py migrate
+docker-compose run web python manage.py createsuperuser --email admin@algoshell.io --username admin
--- setup_db.sh
+++
@@ -1,2 +0,0 @@
-docker-compose run web python manage.py migrate
-docker-compose run web python manage.py createsuperuser --email andrewroblesdev@gmail.com --username andrewrobles
