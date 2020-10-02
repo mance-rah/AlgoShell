10071954c6b43c6320437845e14746f7023e727a
Andrew Robles <andrewroblesdev@gmail.com>
2020-10-01 21:14:03 -0700

    Create test for hello world endpoint

--- tests/tests.py
+++ tests/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+import json
 
-# Create your tests here.
+class RunTestTests(APITestCase):
+    def test_hello_world(self):
+        """
+        Ensure hello world message is returned.
+        """
+        url = reverse('run_test', kwargs={'function_name':'is_palindrome'})
+        expected_response = {'message': 'Hello, World!'}
+        actual_response = self.client.get(url, format='json')
+        self.assertEqual(expected_response, json.loads(actual_response.content))
+        
+
--- tests/urls.py
+++ tests/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from tests import views
 
 urlpatterns = [
-    path('tests/<str:function_name>', views.run_test),
+    path('tests/<str:function_name>', views.run_test, name='run_test'),
 ]
