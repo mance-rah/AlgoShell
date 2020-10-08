3f024b65fc58ddcfb24f4d5715cb6c8dcd9fd346
Andrew Robles <andrewroblesdev@gmail.com>
2020-10-08 08:57:19 -0700

    Automate having to run script that does migration and creates superuser

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 cd backend
 docker-compose up -d --build
+docker-compose run web python manage.py migrate
+docker-compose run web python createsuperuser.py
 
 cd ../frontend
 docker-compose up -d --build
--- setup.sh
+++
@@ -1,3 +0,0 @@
-cd backend
-docker-compose run web python manage.py migrate
-docker-compose run web python manage.py createsuperuser --email admin@algoshell.io --username admin
