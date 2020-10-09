335f4272149d11481315f26cb346e8dd41279727
Andrew Robles <andrewroblesdev@gmail.com>
2020-10-08 20:20:56 -0700

    Write test for get questions api endpoint

--- backend/questions/tests.py
+++ backend/questions/tests.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
+from .models import Question, Category, Difficulty
 import json
 
 class IsPalindromeTests(APITestCase):
@@ -112,6 +113,44 @@ class IsPalindromeTests(APITestCase):
         }
 
         response = self.client.post(url, request_body, format='json')
+        actual_response_body = json.loads(response.content)
+
+        self.assertEqual(expected_response_body, actual_response_body)
+
+class GetQuestionsTests(APITestCase):
+    
+    def setUp(self):
+        easy_difficulty = Difficulty.objects.create(name="Easy")
+        strings_category = Category.objects.create(name="Strings")
+        is_palindrome_question = Question.objects.create(
+            name="Is Palindrome",
+            description="Write a function that determines if a string is a palindrome.",
+            category=strings_category,
+            difficulty=easy_difficulty,
+        )
+
+    def test_is_palindrome_exists(self):
+        """
+        Ensure Is Palindrome endpoint question is returned
+        """
+        url = reverse('get_questions')
+        expected_response_body = [
+            {
+                'title': 'Is Palindrome',
+                'description': 'Write a function that determines if a string is a palindrome.',
+                'function_name': 'is_palindrome',
+                'parameters': ['string'],
+                'category': 'Strings',
+                'difficulty': 'Easy',
+                'tests': [
+                    {'input': ['abcdcba'], 'output': True},
+                    {'input': ['a'], 'output': True},
+                    {'input': ['ab'], 'output': False}
+                ]
+            }
+        ]
+
+        response = self.client.get(url, format='json')
         actual_response_body = json.loads(response.content)
 
         self.assertEqual(expected_response_body, actual_response_body)
