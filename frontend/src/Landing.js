e989e9fe9e43834dade73ec4e276e8ea88a05259
Andrew Robles <andrewroblesdev@gmail.com>
2020-10-19 20:56:12 -0700

    Fonts

--- frontend/src/Landing.js
+++ frontend/src/Landing.js
@@ -4,30 +4,10 @@ import './Landing.css';
 function Landing() {
     return (
         <div className="Landing">
-            <div className="Banner">
-                <p>"It's like LeetCode except it doesn't require the internet"</p>
-            </div>
-            <div className="Navbar">
-                <p>🐚AlgoShell</p>
-            </div>
-            <div className="Headline">
+            <div>
                 <p className="Headline-Title">🐚AlgoShell</p>
                 <p>Shell shock the coding interviews</p>
                 <button className="Button">Start coding</button>
-            </div>
-            <div className="Subheading">
-                <div className="Post">
-                    <div className="Post-Header">
-                        <p>andrewrobles.io</p>
-                        <button className="Button">Follow</button>
-                    </div>
-                    <div className="Post-Description">
-                        <p>It's like leetcode except it doesn't require the internet</p>
-                    </div>
-                    <div className="Post-Song-Title">
-                        <p>Dreams - Fleetwood Mac</p>
-                    </div>
-                </div>
             </div>
         </div>
     )
