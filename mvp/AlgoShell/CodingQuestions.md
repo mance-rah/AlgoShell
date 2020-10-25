cac35f1c5028a9c183fd8e1297e543bc08d968b5
Mance Rah <mance.development@gmail.com>
2020-10-25 00:23:01 -0400

    Add Sentence Reverse

--- mvp/AlgoShell/CodingQuestions.md
+++ mvp/AlgoShell/CodingQuestions.md
@@ -21,3 +21,24 @@ True
 1. Write your solution in `solutions/is_palindrome.py`
 2. Test your solution with `./AlgoShell.sh IsPalindrome`
 
+
+### 2. Sentence Reverse
+You are given an array of characters arr that consists of sequences of characters separated by space characters. Each space-delimited sequence of characters defines a word.
+
+Implement a function reverseWords that reverses the order of the words in the array in the most efficient manner.
+
+**Sample Input:**
+```python
+arr = [ 'p', 'e', 'r', 'f', 'e', 'c', 't', '  ',
+           'm', 'a', 'k', 'e', 's', '  ',
+           'p', 'r', 'a', 'c', 't', 'i', 'c', 'e' ]
+```
+**Sample Output:**
+```python
+[ 'p', 'r', 'a', 'c', 't', 'i', 'c', 'e', '  ',
+  'm', 'a', 'k', 'e', 's', '  ',
+  'p', 'e', 'r', 'f', 'e', 'c', 't' ]
+```
+**Please follow these steps:**
+1. Write your solution in `solutions/sentence_reverse.py`
+2. Test your solution with `./AlgoShell.sh SentenceReverse`
--- mvp/AlgoShell/tests/test_solutions.py
+++ mvp/AlgoShell/tests/test_solutions.py
@@ -74,5 +74,55 @@ class IsPalindrome(unittest.TestCase):
 
         self.assertEqual(expected, actual)
 
+class SentenceReverse(unittest.TestCase):
+    def test_case_1(self):
+        input = [" "," "]
+
+        expected = [" "," "]
+        actual = sentence_reverse(input)
+
+        self.assertEqual(expected, actual)
+
+    def test_case_2(self):
+        input = ["a"," "," ","b"]
+
+        expected = ["b"," "," ","a"]
+        actual = sentence_reverse(input)
+
+        self.assertEqual(expected, actual)
+
+    def test_case_3(self):
+        input = ["h","e","l","l","o"]
+
+        expected = ["h","e","l","l","o"]
+        actual = sentence_reverse(input)
+
+        self.assertEqual(expected, actual)
+
+    def test_case_4(self):
+        input = ["p","e","r","f","e","c","t"," ","m","a","k","e","s"," ","p","r","a","c","t","i","c","e"]
+
+        expected = ["p","r","a","c","t","i","c","e"," ","m","a","k","e","s"," ","p","e","r","f","e","c","t"]
+        actual = sentence_reverse(input)
+
+        self.assertEqual(expected, actual)
+
+    def test_case_5(self):
+        input = ["y","o","u"," ","w","i","t","h"," ","b","e"," ","f","o","r","c","e"," ","t","h","e"," ","m","a","y"]
+
+        expected = ["m","a","y"," ","t","h","e"," ","f","o","r","c","e"," ","b","e"," ","w","i","t","h"," ","y","o","u"]
+        actual = sentence_reverse(input)
+
+        self.assertEqual(expected, actual)
+
+    def test_case_6(self):
+        input = ["g","r","e","a","t","e","s","t"," ","n","a","m","e"," ","f","i","r","s","t"," ","e","v","e","r"," ","n","a","m","e"," ","l","a","s","t"]
+
+        expected = ["l","a","s","t"," ","n","a","m","e"," ","e","v","e","r"," ","f","i","r","s","t"," ","n","a","m","e"," ","g","r","e","a","t","e","s","t"]
+        actual = sentence_reverse(input)
+
+        self.assertEqual(expected, actual)    
+
 if __name__ == '__main__':
     unittest.main()
+
