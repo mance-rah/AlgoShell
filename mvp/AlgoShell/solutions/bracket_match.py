5392825915b9b1e72d7fec3d9d77d9542526f194
Andrew Robles <andrewroblesdev@gmail.com>
2020-10-24 23:01:28 -0700

    Add solutions for is palindrome and bracket match

--- mvp/AlgoShell/solutions/bracket_match.py
+++ mvp/AlgoShell/solutions/bracket_match.py
@@ -1,3 +1,13 @@
 def bracket_match(string):
-	# Write your solution here
-   	pass
+	stack = []
+	count = 0
+
+	for c in string:
+		if c == "(":
+			stack.append(c)
+		elif c == ")" and stack:
+			stack.pop()
+		else:
+			count += 1
+
+	return len(stack) + count
