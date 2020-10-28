c8b4fd2fe28827d8cf71e53310d5c871c1cf36bd
Mance Rah <47769354+mance-rah@users.noreply.github.com>
2020-10-28 13:16:53 -0400

    Basic Landing Page (#39)
    * Add Sentence Reverse
    
    * add sentence_reverse to __init__.py
    
    * Update descriptions
    
    Deleted extra instance of SentenceReverse and add ArrayOfArrayProducts
    
    * Add ArrayOfArrayProducts tests
    
    * Add ArrayOfArrayProducts
    
    Co-authored-by: andrewroblesdev <70864612+andrewroblesdev@users.noreply.github.com>

--- frontend/src/Landing.js
+++ frontend/src/Landing.js
@@ -6,28 +6,35 @@ function Landing() {
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Alegreya+Sans:wght@800&display=swap"></link>
             <nav style={{backgroundColor: "#9580FF"}} class="navbar navbar-light">
                 <div style={{margin: "auto"}} class="navbar-brand">
-                    <p>"It's like LeetCode except it doesn't require the internet"</p>
+                    <p><em>"It's like LeetCode except it doesn't require the internet"</em></p>
                 </div>
             </nav>
 
             <nav style={{backgroundColor: "#282A36"}} class="navbar navbar-dark">
                 <div class="navbar-brand">
-                    <h1 style={{fontFamily: 'Alegreya Sans'}}>AlgoShell</h1>
+                    <h1 style={{fontFamily: 'Alegreya Sans'}}><img id="shelly" src="/sea-snail.png" /> AlgoShell</h1>
                 </div>
             </nav>
 
-            <div style={{backgroundColor:"#373A59", height:"500px"}}>
-                <h1 style={{color: "#50FA7B", fontFamily: 'Alegreya Sans'}}>AlgoShell</h1>
+            <div id="main" style={{backgroundColor:"#373A59", height:"500px"}}>
+                <div>
+                <h1 style={{color: "#50FA7B", fontFamily: 'Alegreya Sans'}}><img src="/sea-snail.png" />AlgoShell</h1>
                 <p style={{color: "white"}}>Shell shock the coding interviews</p>
-                <button class="btn" style={{backgroundColor: "#50FA7B"}}>Start coding</button>
+                <button class="btn-lg" style={{backgroundColor: "#50FA7B"}}>Start Coding</button>
+                </div>
             </div>
 
-            <div style={{backgroundColor:"#282A36", color: "white", height:"500px"}}>
-                <p>It's like LeetCode except it doesn't require the internet.</p>
+            <div id="under-main" style={{backgroundColor:"#282A36", color: "white", height:"720px"}}>
+                <div>
+                <p><em>It's like LeetCode except it doesn't require the internet.</em></p>
+                <iframe width="300" height="500" src="https://www.youtube.com/embed/k-famToMuqY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
             </div>
 
-            <div style={{backgroundColor: "#373A59", height: "150px"}}>
-                <p><span style={{color: "white"}}>AlgoShell</span> <span style={{color: "#50FA7B"}}>Version 1.0</span></p>
+            <div id="footer" style={{backgroundColor: "#373A59", height: "120px"}}>
+                <div>
+                <p><span style={{color: "white"}}><img src="/sea-snail.png" />AlgoShell</span> <span style={{color: "#50FA7B"}}>Version 1.0</span></p>
+                </div>
             </div>
             
         </div>
